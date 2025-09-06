b7d5b7d20b12213cf6652cae087bb4b25f843a43
King Akku <alokak477@gmail.com>
2025-09-06 17:59:36 +0000

    will there be any other issue?

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type {NextConfig} from 'next';
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
