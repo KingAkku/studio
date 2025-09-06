9166c2dab3fc0f4456cced9a563c51350ae202d8
King Akku <alokak477@gmail.com>
2025-09-06 18:45:25 +0000

    Failed to compile. ./next.config.ts:6:5 Type error: Object literal may o

---
+++ src/next.config.ts
@@ -0,0 +1,30 @@
+
+import type {NextConfig} from 'next';
+/** @type {import('next').NextConfig} */
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+};
+
+export default nextConfig;
